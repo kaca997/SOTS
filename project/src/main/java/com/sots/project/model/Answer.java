09acce2dc3f1854c43446bb7f5e2b7f64c774381
kacaa <katarina.aleksic97@gmail.com>
2020-11-25 12:39:58 +0100

    Small changes in model

--- project/src/main/java/com/sots/project/model/Answer.java
+++ project/src/main/java/com/sots/project/model/Answer.java
@@ -22,25 +22,19 @@ public class Answer {
 	@Column(name = "correct")
 	private boolean correct;
 	
-	@Column(name = "selected")
-	private boolean selected;
-
 	public Answer() {
-		this.selected = false;
 	}
 
 	public Answer(String text, boolean correct, boolean selected) {
 		super();
 		this.text = text;
 		this.correct = correct;
-		this.selected = selected;
 	}
 
 	public Answer(String text, boolean correct) {
 		super();
 		this.text = text;
 		this.correct = correct;
-		this.selected = false;
 	}
 
 	public Long getId() {
@@ -65,14 +59,6 @@ public class Answer {
 
 	public void setCorrect(boolean correct) {
 		this.correct = correct;
-	}
-
-	public boolean isSelected() {
-		return selected;
-	}
-
-	public void setSelected(boolean selected) {
-		this.selected = selected;
 	}
 	
 }
--- project/src/main/java/com/sots/project/model/DoneTest.java
+++ project/src/main/java/com/sots/project/model/DoneTest.java
@@ -1,5 +1,8 @@
 package com.sots.project.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +30,10 @@ public class DoneTest {
 	@ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="student_id")
 	private Student student;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "done_test_id")
+	private List<Answer> chosenAnswers;
 
 	public DoneTest() {
 		super();
